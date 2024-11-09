86289058d86769de9b523d0a97a39d1dae9c6079
kokizzu <kokizzu@users.noreply.github.com>
2024-11-09 16:11:41 +0000

    deploy: d8401a8f138cd506086f47b572c9e822c7f6673a

---
+++ _app/immutable/chunks/paths.Li1vEPa3.js
@@ -0,0 +1,1 @@
+var e;const a=((e=globalThis.__sveltekit_3ezeg)==null?void 0:e.base)??"/mermaid-live-editor";var s;const t=((s=globalThis.__sveltekit_3ezeg)==null?void 0:s.assets)??a;export{t as a,a as b};
